b5ec7822138bf8d25b9481606f09ad3c6dd8a529
sangle <15122004sl@gmail.com>
2026-03-31 22:57:34 +0700

    Delete bài 3 directory

--- bài 3/Course.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class Course extends Model {
-    protected $table = 'mon_hoc';
-    public $timestamps = false;
-    protected $fillable = ['ten_mon', 'so_tin_chi'];
-}
